8418c3bd38f52f8a10ed81389b76a92ebe4e84bd
abhishek <babbi.abhishek@gmail.com>
2018-07-15 19:29:50 +0530

    - preferencesummary added

--- app/src/main/java/com/example/abhishek/weatherforecast/settings/SettingsFragment.java
+++ app/src/main/java/com/example/abhishek/weatherforecast/settings/SettingsFragment.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,10 +22,52 @@ import com.example.abhishek.weatherforecast.R;
 public class SettingsFragment extends PreferenceFragmentCompat
         implements SharedPreferences.OnSharedPreferenceChangeListener{
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getPreferenceScreen()
+                .getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getPreferenceScreen()
+                .getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.user_pref);
 
+        SharedPreferences sharedPreferences = getPreferenceScreen()
+                .getSharedPreferences();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+
+        for(int i=0; i<preferenceScreen.getPreferenceCount(); i++){
+            Preference preference = preferenceScreen.getPreference(i);
+            if(!(preference instanceof CheckBoxPreference)){
+                String value = sharedPreferences.getString(preference
+                        .getKey(),"");
+                setPreferenceSummary(preference, value);
+            }
+
+        }
+    }
+
+    private void setPreferenceSummary(Preference preference, Object value) {
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(value.toString());
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        }else {
+            preference.setSummary(value.toString());
+        }
     }
 
     @Override
