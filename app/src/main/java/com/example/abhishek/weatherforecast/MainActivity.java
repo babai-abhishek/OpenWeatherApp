9ac37510a2b1c5256e8f89ac88dd67ee0079dedd
abhishek <babbi.abhishek@gmail.com>
2018-07-15 13:51:24 +0530

    - weatherlistfragment added

--- app/src/main/java/com/example/abhishek/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/abhishek/weatherforecast/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.abhishek.weatherforecast;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG_FRAGMENT_WEATHER_LIST = "weatherListFragment";
+    private FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.flFragmentContainer, new WeatherListFragment(),TAG_FRAGMENT_WEATHER_LIST);
+        transaction.commit();
     }
 }
