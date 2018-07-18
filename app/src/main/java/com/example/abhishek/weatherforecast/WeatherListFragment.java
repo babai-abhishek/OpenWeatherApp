f83bcfe1d6e1909191b6ab7daa202651b9033750
abhishek <babbi.abhishek@gmail.com>
2018-07-18 19:15:31 +0530

    - broascastreceiver added .

--- app/src/main/java/com/example/abhishek/weatherforecast/WeatherListFragment.java
+++ app/src/main/java/com/example/abhishek/weatherforecast/WeatherListFragment.java
@@ -1,11 +1,15 @@
 package com.example.abhishek.weatherforecast;
 
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,13 +17,16 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 import com.example.abhishek.weatherforecast.model.api.WeatherApiModel;
+import com.example.abhishek.weatherforecast.model.api.WeatherListApiModel;
 import com.example.abhishek.weatherforecast.networkutils.ApiClient;
 import com.example.abhishek.weatherforecast.networkutils.WeatherInterface;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import retrofit2.Call;
@@ -34,11 +41,37 @@ public class WeatherListFragment extends Fragment
         implements SharedPreferences.OnSharedPreferenceChangeListener{
 
     private static final String OWM_API_KEY = "71ecdcdd6d04f99f1c06210c95011f10";
+    private static final String ACTION_WEATHER_FORECAST_API_SUCCESS = "com.example.abhishek.weatherforecast.weatherlistfragment.api.weatherforecast.result.success";
+    private static final String ACTION_WEATHER_FORECAST_API_FAILURE = "com.example.abhishek.weatherforecast.weatherlistfragment.api.weatherforecast.result.fail";
+    private static final String KEY_WEATHER_FORECAST = "weather_forecast";
 
     SettingsOptionClickListener clickListener;
-
-    List<WeatherApiModel> weatherDetails = new ArrayList<>();
+    private LocalBroadcastManager broadcastManager = null;
     WeatherInterface weatherInterface = ApiClient.getClient().create(WeatherInterface.class);
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            switch (intent.getAction()) {
+                case ACTION_WEATHER_FORECAST_API_SUCCESS:
+                    Toast.makeText(getActivity(), "Api Success", Toast.LENGTH_SHORT).show();
+                    WeatherApiModel weather = intent.getParcelableExtra(KEY_WEATHER_FORECAST);
+                    List<WeatherListApiModel> list = weather.getWeatherListApiModel();
+                    for(WeatherListApiModel apiModel: list){
+                        Log.d("#","dt = "+apiModel.getDt());
+                    }
+                    break;
+
+
+                case ACTION_WEATHER_FORECAST_API_FAILURE:
+                    Toast.makeText(getActivity(), "Api Failure", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+
+        }
+    };
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -57,7 +90,7 @@ public class WeatherListFragment extends Fragment
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        broadcastManager = LocalBroadcastManager.getInstance(getActivity());
         setHasOptionsMenu(true);
         loadWeather();
     }
@@ -77,11 +110,27 @@ public class WeatherListFragment extends Fragment
 
         return super.onOptionsItemSelected(item);
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_weather_list, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_WEATHER_FORECAST_API_SUCCESS);
+        filter.addAction(ACTION_WEATHER_FORECAST_API_FAILURE);
+        broadcastManager.registerReceiver(broadcastReceiver, filter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        broadcastManager.unregisterReceiver(broadcastReceiver);
     }
 
     @Override
@@ -98,12 +147,27 @@ public class WeatherListFragment extends Fragment
          call.enqueue(new Callback<WeatherApiModel>() {
             @Override
             public void onResponse(Call<WeatherApiModel> call, Response<WeatherApiModel> response) {
-                Log.d("#","response "+response.body().toString());
+                WeatherApiModel weather = null;
+
+               //if response is sucessful
+                if(response.isSuccessful()){
+                    //get data from response
+                    weather = response.body();
+                }
+
+               //register intent for broadcast manager
+                Intent intent = new Intent(ACTION_WEATHER_FORECAST_API_SUCCESS);
+                intent.putExtra(KEY_WEATHER_FORECAST,weather);
+
+                //send broadcast
+                broadcastManager.sendBroadcast(intent);
+
             }
 
             @Override
             public void onFailure(Call<WeatherApiModel> call, Throwable t) {
-
+                Intent intent = new Intent(ACTION_WEATHER_FORECAST_API_FAILURE);
+                broadcastManager.sendBroadcast(intent);
             }
         });
 
